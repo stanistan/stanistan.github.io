93bedefe2ca755c7068a89b43898aaf6633c8539
Stan Rozenraukh <stan@stanistan.com>
2019-07-16 12:50:20 -0400

    with things

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # stanistan
 
 Personal website. Powered by [zola](https://getzola.org).
-
-
---
+++ ship.sh
@@ -0,0 +1,3 @@
+# ships `public/`
+# based on https://gist.github.com/cobyism/4730490
+git subtree push --prefix public origin gh-pages
