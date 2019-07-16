2f48bf1e84407a76300bf3710f73ef559d8444e3
Stan Rozenraukh <stan@stanistan.com>
2019-07-16 14:06:32 -0400

    update ship & README

--- README.md
+++ README.md
@@ -14,18 +14,6 @@ zola build
 git add ...
 git commit 
 
-# push upstream
-git push origin the-details
-
-# push the changes to master...
-# master is where/how the site actually works,
-# powering stanistan.github.io -> www.stanistan.com
-git branch -D master
-git subtree split --prefix public -b master
-git checkout master
-git pull origin master --rebase
-git push origin master
-git checkout the-details
-
-# or ./ship.sh
+# ship everything from ./public to the master branch
+./ship.sh
 ```
--- ship.sh
+++ ship.sh
@@ -1,9 +1,24 @@
-set -ex
 # ships `public/`
 # based on https://gist.github.com/cobyism/4730490
+set -ex
+
+# make sure everything we care about has already been 
+# pushed upstream 
+git push origin the-details
+
+# removes the master branch, and splits the subtree
+# into it.
 git branch -D master
 git subtree split --prefix public -b master
+
+# we do this dance because the master
+# branch would have changed with the cname
+# things from having the site up on github
+# pages, and... we don't really want to squash
+# that CNAME
 git checkout master
 git pull origin master --rebase
 git push origin master
+
+# go back to our working branch
 git checkout the-details
