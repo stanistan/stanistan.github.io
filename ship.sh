e658643f04011ecf88150f568ec9c90067c2f880
Stan Rozenraukh <stan@stanistan.com>
2019-07-16 13:59:10 -0400

    updating ship.sh

--- ship.sh
+++ ship.sh
@@ -1,3 +1,9 @@
+set -ex
 # ships `public/`
 # based on https://gist.github.com/cobyism/4730490
-git subtree push --prefix public origin master
+git branch -D master
+git subtree split --prefix public -b master
+git checkout master
+git pull origin master --rebase
+git push origin master
+git checkout the-details
